f2b4ee6f4579043c5ec497c32c8c6a673b1381b0
JORDHI12 <110746648+JORDHI12@users.noreply.github.com>
2025-11-01 22:31:48 +0700

    Refactor Select component and improve structure

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import * as React from "react"
+import { Check, ChevronDown } from "lucide-react"
 import * as SelectPrimitive from "@radix-ui/react-select"
-
 import { cn } from "@/lib/utils"
 
 const Select = SelectPrimitive.Root
@@ -23,6 +23,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
+      <ChevronDown className="h-4 w-4 opacity-50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ))
@@ -31,21 +32,17 @@ SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
+>(({ className, children, ...props }, ref) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
         "relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md animate-in fade-in-80",
-        position === "popper" && "translate-y-1",
         className
       )}
-      position={position}
       {...props}
     >
-      <SelectPrimitive.Viewport
-        className={cn("p-1", position === "popper" && "p-2")}
-      >
+      <SelectPrimitive.Viewport className="p-1">
         {children}
       </SelectPrimitive.Viewport>
     </SelectPrimitive.Content>
@@ -65,9 +62,21 @@ const SelectItem = React.forwardRef<
     )}
     {...props}
   >
+    <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
+      <SelectPrimitive.ItemIndicator>
+        <Check className="h-4 w-4" />
+      </SelectPrimitive.ItemIndicator>
+    </span>
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
   </SelectPrimitive.Item>
 ))
 SelectItem.displayName = SelectPrimitive.Item.displayName
 
-export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectItem }
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+}
